a863b5d0d556dd9c40305e5c54179c1c8020aa4c
SEELE1306 <mkdang@proton.me>
2024-09-10 10:05:08 +0700

    Add packages and DE i8n

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,12 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  // internationalization (i18n) configuration
+  i18n: {
+    defaultLocale: "en",
+    locales: ["de", "en"],
+    routing: {
+      prefixDefaultLocale: false,
+    },
+  },
+});
